55e1f069f445e9aabb61fdc1da1f6f400db8dfa8
Anders Sundin <olsu2201@student.miun.se>
2024-02-05 11:21:17 +0100

    Rensar upp bland bloat-kod

--- obj/Debug/net8.0/mom4.AssemblyInfo.cs
+++ obj/Debug/net8.0/mom4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mom4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5c0817dde74a008125c201abbd43a26ec79a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("mom4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mom4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
