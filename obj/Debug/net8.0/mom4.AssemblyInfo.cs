754113aeccef42ff854d1d88a11a70ddfe96eb5c
Anders Sundin <olsu2201@student.miun.se>
2024-02-05 18:23:26 +0100

    skapar en anslutning till db

--- obj/Debug/net8.0/mom4.AssemblyInfo.cs
+++ obj/Debug/net8.0/mom4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mom4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9433c506fe68e951dfe85aff3b843b89303a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("mom4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mom4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
